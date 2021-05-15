85e96bae8313a0f295c8f1a869a24ec79152b60c
Patrycja Kaźmierczak <patrycja.kazmierczak1996@gmail.com>
2021-05-15 12:23:58 +0200

    commit

---
+++ js/script.js
@@ -0,0 +1,92 @@
+
+{
+    const tasks = [
+    ];
+
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+
+        render();
+    }
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    }
+
+    const toggleTaskDone = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done;
+        render();
+    }
+
+    const bindEvents = () => {
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(index);
+            });
+        });
+
+        const toggleDoneButtons = document.querySelectorAll(".js-done");
+
+        toggleDoneButtons.forEach((toggleDoneButton, index) => {
+            toggleDoneButton.addEventListener("click", () => {
+                toggleTaskDone(index);
+            });
+        });
+    }
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+            <li class="list__item">
+            <button class="js-done list__button list__button--done">${task.done ? "&#x2714;" : " "}</button>
+            <span class="list__paragraph ${task.done ? "list__paragraph--done\"" : ""}">${task.content}</span> 
+            <button class="js-remove list__button list__button--remove">&#128465;</button>
+            </li>
+            `;
+        }
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        bindEvents();
+
+    };
+
+    const resetFormInput = (newTask) => {
+        newTask.value = "";
+        newTask.focus();
+    };
+
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTask = document.querySelector(".js-newTask");
+        const newTaskContent = newTask.value.trim();
+
+        if (newTaskContent === "") {
+            resetFormInput(newTask);
+            return;
+        }
+
+        addNewTask(newTaskContent);
+        resetFormInput(newTask);
+    };
+
+
+    const init = () => {
+        render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
+    };
+
+    init();
+
+}
